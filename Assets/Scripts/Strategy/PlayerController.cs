4a2d0cc8a8fb8222e365b175b8e5609fb906eadc
benherbs <benjamin.berbrier@mail.mcgill.ca>
2025-02-28 13:10:48 -0500

    bad commit 2

--- Assets/Scripts/Strategy/PlayerController.cs
+++ Assets/Scripts/Strategy/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //helloworld
         
     }
 
