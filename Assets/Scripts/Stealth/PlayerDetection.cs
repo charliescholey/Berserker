8d56f5795c4a408ea6eb3b965bfece52cecf7ed2
EricWinter7 <139814693+EricWinter7@users.noreply.github.com>
2025-03-12 21:46:54 -0400

    rotating and walking enemies w/ player detection - no script to start combat mode yet
    created prefabs for rotating enemies (one for each direction - up, down, left right) and for walking enemies (one for each direction - vertical and horizontal)
    
    Enemy vision cones have colliders and will detect players but just prints to log if player was detected ***NEED TO INSERT SCRIPT TO START COMBAT WITHIN PlayerDetection SCRIPT***
    
    didn't change player movement for other characters to follow leader yet
    
    added a tag for players so they can be detected by enemies

---
+++ Assets/Scripts/Stealth/PlayerDetection.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player")) {
+            Debug.Log("Player has been spotted by enemy character!");
+        }
+    }
+}
