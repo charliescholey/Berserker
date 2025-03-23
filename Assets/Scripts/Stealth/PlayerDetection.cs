65f0bdf29629a4f25b8d66b705128f7f9666f806
Ariel Khoutorsky <ariel.khoutorsky@mail.mcgill.ca>
2025-03-23 04:50:11 -0400

    added transitions between strategy and stealth: on enemy kill and death

--- Assets/Scripts/Stealth/PlayerDetection.cs
+++ Assets/Scripts/Stealth/PlayerDetection.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class PlayerDetection : MonoBehaviour {
+public class PlayerDetection : MonoBehaviour
+{
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("player")) {
+        if (collision.CompareTag("player"))
+        {
             Debug.Log("Player has been spotted by enemy character!");
-            SceneManager.LoadScene("Strategy", LoadSceneMode.Single);
+            
+            // Traverse up the parent hierarchy to destroy the enemy root object
+            Transform current = transform;
+            while (current != null)
+            {
+                if (current.CompareTag("EnemyRoot"))
+                {
+                    Destroy(current.gameObject);
+                    break;
+                }
+                current = current.parent;
+            }
+            
+            StealthManager.Instance.LoadAndActivateScene("Strategy");
         }
     }
 }
