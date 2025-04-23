953543450a9ec63bb0313cab0c52ba55672ec6d1
Charlie Scholey <charlieoscholey@gmail.com>
2025-04-22 21:32:23 -0400

    walls have tiles now

--- Assets/Scripts/Strategy/GameManager.cs
+++ Assets/Scripts/Strategy/GameManager.cs
@@ -75,6 +75,7 @@ public class GameManager : MonoBehaviour
             new Vector2Int(-2, 1),
             new Vector2Int(-2, 0),
             new Vector2Int(-1, 0),
+            new Vector2Int(-1, -1)
         };
         boardManager.setWalls(walls);
     }
