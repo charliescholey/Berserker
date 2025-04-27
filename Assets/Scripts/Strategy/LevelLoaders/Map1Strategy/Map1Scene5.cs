2e449f3338a7281a7518d32bcf85063949c66b69
benherbs <benjamin.berbrier@mail.mcgill.ca>
2025-04-27 19:26:30 -0400

    Levelcreation (#37)
    * added first strategy level for map 1
    
    * naming conventions
    
    * map 1 scene 2 strategy map done
    
    * map 1 strategy scene 3
    
    * map 1 strategy scene 4
    
    * map 1 strategy dcene 5, final scene
    
    * added all map 2 strategy templates, made map 2
    strategy scene 1
    
    * map 2 strategy scene 2
    
    * map 2 strategy scene 3
    
    * map 2 strategy scene 4
    
    * map 2 strategy scene 5 mostly functions ask charli
    
    * Stealth lvl 1 - PURELY VISUAL, NO CODE/LOADING
    
    * map 3 strategy scene 1
    
    * map 3 Strategy Scene 1 (last one shouldve been 4)
    
    * map 3 strategy scene 2
    
    * fix for map 2 scene 5
    
    * map 3 strategy scene 3
    
    * map 3 Strategy scene 5, last one!
    
    ---------
    
    Co-authored-by: EricWinter7 <139814693+EricWinter7@users.noreply.github.com>
    Co-authored-by: Charlie Scholey <charlieoscholey@gmail.com>

---
+++ Assets/Scripts/Strategy/LevelLoaders/Map1Strategy/Map1Scene5.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Map1Scene5 : MonoBehaviour
+{
+    public GameManager level;
+    public BoardManager map;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        //PLAYER SPAWN LOCATIONS
+        Vector2Int[] playerSpawnLocations = {
+            new Vector2Int(7, 2),
+            new Vector2Int(7, 1),
+            new Vector2Int(7, 0),
+            new Vector2Int(7, -1)
+        };
+
+        map.setSpawnLocations(playerSpawnLocations);
+
+        //ENEMY SPAWN LOCATIONS
+        Vector2Int[] enemySpawnLocations = {
+            new Vector2Int(4,0),
+            new Vector2Int(4,3),
+            new Vector2Int(-1,0),
+            new Vector2Int(-1,3),
+            new Vector2Int(-6,0),
+            new Vector2Int(-6,3),
+            
+        };
+        level.enemySpawnLocations = enemySpawnLocations;
+
+        level.spawnEnemies(enemySpawnLocations);
+
+        // WALLS
+        List<Vector2Int> walls = new List<Vector2Int>();
+
+        // horizontal wall on top  
+        walls.AddRange(generateBox(new Vector2Int(-10, 4), new Vector2Int(6, 4)));
+
+        // horizontal wall bottom 
+        walls.AddRange(generateBox(new Vector2Int(-7, -3), new Vector2Int(8, -3)));
+
+        //vertical wall on left
+        walls.AddRange(generateBox(new Vector2Int(-10,3), new Vector2Int(-10,-3)));
+
+        //vertical wall on bottom
+        walls.AddRange(generateBox(new Vector2Int(9,4), new Vector2Int(9,-5)));
+
+        //benches
+        walls.AddRange(generateBox(new Vector2Int(3,-1), new Vector2Int(5,-1)));
+
+        walls.AddRange(generateBox(new Vector2Int(3,2), new Vector2Int(5,2)));
+
+        walls.AddRange(generateBox(new Vector2Int(-2,-1), new Vector2Int(0,-1)));
+
+        walls.AddRange(generateBox(new Vector2Int(-2,2), new Vector2Int(0,2)));
+
+        walls.AddRange(generateBox(new Vector2Int(-7,-1), new Vector2Int(-5,-1)));
+
+        walls.AddRange(generateBox(new Vector2Int(-7,2), new Vector2Int(-5,2)));
+
+        map.setWalls(walls.ToArray());
+    }
+
+
+    List<Vector2Int> generateBox(Vector2Int topLeft, Vector2Int bottomRight)
+    {
+        Debug.Log("Generating box from " + topLeft + " to " + bottomRight);
+        List<Vector2Int> box = new List<Vector2Int>();
+        for (int x = topLeft.x; x <= bottomRight.x; x++)
+        {
+            for (int y = topLeft.y; y >= bottomRight.y; y--)
+            {
+                box.Add(new Vector2Int(x, y));
+            }
+        }
+        Debug.Log("Box generated with " + box.Count + " tiles.");
+        return box;
+    }
+}
