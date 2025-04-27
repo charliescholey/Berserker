82f6a9883052c3fa96aec27b1cd3c6c25fd2b2da
benherbs <benjamin.berbrier@mail.mcgill.ca>
2025-04-27 17:21:42 -0400

    fix for map 2 scene 5

--- Assets/Scripts/Strategy/LevelLoaders/Map2Strategy/Map2Scene5.cs
+++ Assets/Scripts/Strategy/LevelLoaders/Map2Strategy/Map2Scene5.cs
@@ -35,6 +35,8 @@ public class Map2Scene5 : MonoBehaviour
         // WALLS
         List<Vector2Int> walls = new List<Vector2Int>();
 
+        walls.AddRange(generateBox(new Vector2Int(4,-2), new Vector2Int(5, -2)));
+
         // First wall on top
         walls.AddRange(generateBox(new Vector2Int(-10, 4), new Vector2Int(9, 4)));
 
@@ -45,43 +47,49 @@ public class Map2Scene5 : MonoBehaviour
         walls.AddRange(generateBox(new Vector2Int(9, 3), new Vector2Int(9, -4)));
 
         //bottom right wall
-        walls.AddRange(generateBox(new Vector2Int(9, -2), new Vector2Int(9, -4)));
+        /*walls.AddRange(generateBox(new Vector2Int(9, -2), new Vector2Int(9, -4)));
 
         //top right wall
-        walls.AddRange(generateBox(new Vector2Int(9, 3), new Vector2Int(9, 1)));
+        walls.AddRange(generateBox(new Vector2Int(9, 3), new Vector2Int(9, 1)));*/
 
-        //none of this code is wokring ask charlie why
-        // //cell doors top(horizontal walls)
-        // walls.AddRange(generateBox(new Vector2Int(-9,1), new Vector2Int(-8,1)));
+        //NEW WALLS
+        //upper cell
+        walls.AddRange(generateBox(new Vector2Int(4,3), new Vector2Int(4,1)));
+        walls.AddRange(generateBox(new Vector2Int(5,1), new Vector2Int(7,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(-5,1), new Vector2Int(-3,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(0,1), new Vector2Int(2,1)));
+        //cell doors top(horizontal walls)
+        walls.AddRange(generateBox(new Vector2Int(-9,1), new Vector2Int(-8,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(5,1), new Vector2Int(7,1)));
+        walls.AddRange(generateBox(new Vector2Int(-5,1), new Vector2Int(-3,1)));
 
-        // //cell doors bottom(horizontal walls)
-        // walls.AddRange(generateBox(new Vector2Int(-9,-2), new Vector2Int(-8,-2)));
+        walls.AddRange(generateBox(new Vector2Int(0,1), new Vector2Int(2,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(-5,-2), new Vector2Int(-3,-2)));
+        walls.AddRange(generateBox(new Vector2Int(5,1), new Vector2Int(7,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(0,-2), new Vector2Int(2,-2)));
+        //cell doors bottom(horizontal walls)
+        walls.AddRange(generateBox(new Vector2Int(-9,-2), new Vector2Int(-8,-2)));
 
-        // walls.AddRange(generateBox(new Vector2Int(5,-2), new Vector2Int(7,-2)));
+        walls.AddRange(generateBox(new Vector2Int(-5,-2), new Vector2Int(-3,-2)));
 
-        // //vetical cell walls top
-        // walls.AddRange(generateBox(new Vector2Int(-6,3), new Vector2Int(-6,1)));
+        walls.AddRange(generateBox(new Vector2Int(0,-2), new Vector2Int(2,-2)));
 
-        // walls.AddRange(generateBox(new Vector2Int(-1,3), new Vector2Int(-1,1)));
+        walls.AddRange(generateBox(new Vector2Int(5,-2), new Vector2Int(7,-2)));
 
-        // walls.AddRange(generateBox(new Vector2Int(4,3), new Vector2Int(4,1)));
+        //vetical cell walls top
+        walls.AddRange(generateBox(new Vector2Int(-6,3), new Vector2Int(-6,1)));
 
-        // //vetical cell walls bottom
-        // walls.AddRange(generateBox(new Vector2Int(-6,-2), new Vector2Int(7,-4)));
+        walls.AddRange(generateBox(new Vector2Int(-1,3), new Vector2Int(-1,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(-1,-2), new Vector2Int(-1,-4)));
+        walls.AddRange(generateBox(new Vector2Int(4,3), new Vector2Int(4,1)));
 
-        // walls.AddRange(generateBox(new Vector2Int(4,-2), new Vector2Int(4,-4)));
+        //vetical cell walls bottom
+        walls.AddRange(generateBox(new Vector2Int(-6,-2), new Vector2Int(7,-4)));
+
+        walls.AddRange(generateBox(new Vector2Int(-1,-2), new Vector2Int(-1,-4)));
+
+        walls.AddRange(generateBox(new Vector2Int(4,-2), new Vector2Int(4,-4)));
+
 
 
         map.setWalls(walls.ToArray());
@@ -91,6 +99,10 @@ public class Map2Scene5 : MonoBehaviour
     List<Vector2Int> generateBox(Vector2Int topLeft, Vector2Int bottomRight)
     {
         Debug.Log("Generating box from " + topLeft + " to " + bottomRight);
+        //error logging
+        if(topLeft.x > bottomRight.x || topLeft.y < bottomRight.y){
+            Debug.LogError("Incorrect usage of GenerateBox starting at " + topLeft);
+        }
         List<Vector2Int> box = new List<Vector2Int>();
         for (int x = topLeft.x; x <= bottomRight.x; x++)
         {
