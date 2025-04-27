81e6354d53038bd248e6e51fb17edc303988f920
benherbs <benjamin.berbrier@mail.mcgill.ca>
2025-04-27 19:04:16 -0400

    map 3 Strategy scene 5, last one!

--- Assets/Scripts/Strategy/LevelLoaders/Map3Strategy/Map3Scene5.cs
+++ Assets/Scripts/Strategy/LevelLoaders/Map3Strategy/Map3Scene5.cs
@@ -10,20 +10,18 @@ public class Map3Scene5 : MonoBehaviour
     {
         //PLAYER SPAWN LOCATIONS
         Vector2Int[] playerSpawnLocations = {
-            new Vector2Int(4, -1),
-            new Vector2Int(5, -1),
-            new Vector2Int(6, -1),
-            new Vector2Int(7, -1)
+            new Vector2Int(-9, 3),
+            new Vector2Int(-9, 1),
+            new Vector2Int(-9, -1),
+            new Vector2Int(-9, -3)
         };
 
         map.setSpawnLocations(playerSpawnLocations);
 
         //ENEMY SPAWN LOCATIONS
         Vector2Int[] enemySpawnLocations = {
-            new Vector2Int(0,0),
-            new Vector2Int(2,0),
-            new Vector2Int(5,3),
-            new Vector2Int(7,3),
+            new Vector2Int(-7,-2),
+            new Vector2Int(-7,1),
         };
         level.enemySpawnLocations = enemySpawnLocations;
 
@@ -33,33 +31,21 @@ public class Map3Scene5 : MonoBehaviour
         // for this scene only making walls for the house we are in
         List<Vector2Int> walls = new List<Vector2Int>();
 
-        // First wall on top
-        walls.AddRange(generateBox(new Vector2Int(-2, 4), new Vector2Int(9, 4)));
+        // First wall on bottom big one
+        walls.AddRange(generateBox(new Vector2Int(-2, -5), new Vector2Int(9, -5)));
 
-        // left wall
-        walls.AddRange(generateBox(new Vector2Int(-2, 3), new Vector2Int(-2, -3)));
+        //top left wall
+        walls.AddRange(generateBox(new Vector2Int(-6, 4), new Vector2Int(-6, 1)));
 
-        // right wall
-        walls.AddRange(generateBox(new Vector2Int(9, 3), new Vector2Int(9, -3)));
+        //bottom left wall
+        walls.AddRange(generateBox(new Vector2Int(-6, -2), new Vector2Int(-6, -5)));
 
-        //bottom wall left 
-        walls.AddRange(generateBox(new Vector2Int(-1, -3), new Vector2Int(2, -3)));
+        //tiny wall
+        walls.AddRange(generateBox(new Vector2Int(5, -5), new Vector2Int(5, -5)));
 
-        //bottom wall right 
-        walls.AddRange(generateBox(new Vector2Int(5, -3), new Vector2Int(8, -3)));
+        //invisible outside wall
+        walls.AddRange(generateBox(new Vector2Int(-11, 4), new Vector2Int(-11, -5)));
 
-        //benches
-        walls.AddRange(generateBox(new Vector2Int(0, -1), new Vector2Int(2, -1)));
-
-        walls.AddRange(generateBox(new Vector2Int(5, 2), new Vector2Int(7, 2)));
-
-        //corner knook
-        walls.AddRange(generateBox(new Vector2Int(-1, 1), new Vector2Int(0, 1)));
-
-        walls.AddRange(generateBox(new Vector2Int(1, 2), new Vector2Int(1, 1)));
-
-        //car just because
-        walls.AddRange(generateBox(new Vector2Int(-1, -4), new Vector2Int(0, -4)));
 
 
         map.setWalls(walls.ToArray());
