d0146b7ee4a8747d871aef8aa0497cc5e6369607
benherbs <benjamin.berbrier@mail.mcgill.ca>
2025-04-27 12:27:29 -0400

    map 2 strategy scene 2

--- Assets/Scripts/Strategy/LevelLoaders/Map2Strategy/Map2Scene2.cs
+++ Assets/Scripts/Strategy/LevelLoaders/Map2Strategy/Map2Scene2.cs
@@ -10,18 +10,18 @@ public class Map2Scene2 : MonoBehaviour
     {
         //PLAYER SPAWN LOCATIONS
         Vector2Int[] playerSpawnLocations = {
-            new Vector2Int(-6, 1),
-            new Vector2Int(-4, 1),
-            new Vector2Int(-6, -1),
-            new Vector2Int(-4, -1)
+            new Vector2Int(2, -5),
+            new Vector2Int(0, -5),
+            new Vector2Int(-2, -5),
+            new Vector2Int(-4, -5)
         };
 
         map.setSpawnLocations(playerSpawnLocations);
 
         //ENEMY SPAWN LOCATIONS
         Vector2Int[] enemySpawnLocations = {
-            new Vector2Int(-1,-2),
-            new Vector2Int(-1,1),
+            new Vector2Int(1,-3),
+            new Vector2Int(-2,-3),
             
         };
         level.enemySpawnLocations = enemySpawnLocations;
@@ -31,11 +31,17 @@ public class Map2Scene2 : MonoBehaviour
         // WALLS
         List<Vector2Int> walls = new List<Vector2Int>();
 
-        // First wall on top
-        walls.AddRange(generateBox(new Vector2Int(0, 4), new Vector2Int(0, 1)));
+        // bottom left
+        walls.AddRange(generateBox(new Vector2Int(-10, -2), new Vector2Int(-2, -2)));
 
         // Second wall on bottom
-        walls.AddRange(generateBox(new Vector2Int(0, -2), new Vector2Int(0, -5)));
+        walls.AddRange(generateBox(new Vector2Int(1, -2), new Vector2Int(9,-2)));
+
+        //vertical wall left  
+        walls.AddRange(generateBox(new Vector2Int(-6, 3), new Vector2Int(-6, -1)));
+
+        //vertical walls right 
+        walls.AddRange(generateBox(new Vector2Int(3, 3), new Vector2Int(3, -1)));
 
         map.setWalls(walls.ToArray());
     }
